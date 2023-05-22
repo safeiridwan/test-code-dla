563cd5e9cf74ef5fd97fc404a2f84a09e4dbc205
Ahmad Safei R <ahmad.ridwan@nexsoft.co.id>
2023-05-22 16:02:04 +0700

    initial project spring

---
+++ dla/src/main/java/com/project/dla/DlaApplication.java
@@ -0,0 +1,13 @@
+package com.project.dla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlaApplication.class, args);
+	}
+
+}
---
+++ dla/src/test/java/com/project/dla/DlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
